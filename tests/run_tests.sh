6602b89750c47b2d7522b56eb1f380e39def4720
Domingo Dirutigliano <me@domy.sh>
2024-04-09 04:17:45 +0200

    run test return error

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,27 +4,30 @@ cd "$(dirname "$0")"
 
 TMP=$1
 PASSWORD=${TMP:=testpassword}
+ERROR=0
 
 pip3 install -r requirements.txt
 
 echo "Running standard API test"
-python3 api_test.py -p $PASSWORD
+python3 api_test.py -p $PASSWORD || ERROR=1
 echo "Running Netfilter Regex TCP ipv4"
-python3 nf_test.py -p $PASSWORD -m tcp
+python3 nf_test.py -p $PASSWORD -m tcp || ERROR=1
 echo "Running Netfilter Regex TCP ipv6"
-python3 nf_test.py -p $PASSWORD -m tcp -6
+python3 nf_test.py -p $PASSWORD -m tcp -6 || ERROR=1
 echo "Running Netfilter Regex UDP ipv4"
-python3 nf_test.py -p $PASSWORD -m udp
+python3 nf_test.py -p $PASSWORD -m udp || ERROR=1
 echo "Running Netfilter Regex UDP ipv6"
-python3 nf_test.py -p $PASSWORD -m udp -6
+python3 nf_test.py -p $PASSWORD -m udp -6 || ERROR=1
 echo "Running Proxy Regex"
-python3 px_test.py -p $PASSWORD
+python3 px_test.py -p $PASSWORD || ERROR=1
 echo "Running Port Hijack TCP ipv4"
-python3 ph_test.py -p $PASSWORD -m tcp
+python3 ph_test.py -p $PASSWORD -m tcp || ERROR=1
 echo "Running Port Hijack TCP ipv6"
-python3 ph_test.py -p $PASSWORD -m tcp -6
+python3 ph_test.py -p $PASSWORD -m tcp -6 || ERROR=1
 echo "Running Port Hijack UDP ipv4"
-python3 ph_test.py -p $PASSWORD -m udp
+python3 ph_test.py -p $PASSWORD -m udp || ERROR=1
 echo "Running Port Hijack UDP ipv6"
-python3 ph_test.py -p $PASSWORD -m udp -6
+python3 ph_test.py -p $PASSWORD -m udp -6 || ERROR=1
+
+exit $ERROR
 
